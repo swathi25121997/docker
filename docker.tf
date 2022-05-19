84b5884ed4fd3838b722f559542bb0d6ffbf445b
swathi25121997 <104359251+swathi25121997@users.noreply.github.com>
2022-05-19 13:25:49 +0530

    docker file

---
+++ docker.tf
@@ -0,0 +1,49 @@
+provider "aws" {
+    region     = "ap-south-1"
+    access_key = "AKIA24LGQVQKZSQRYCG5"
+    secret_key = "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"
+
+}
+resource "aws_instance" "app_server" { 
+
+    ami = "ami-079b5e5b3971bd10d"
+    instance_type = "t2.micro"
+}
+data "aws_ami" "ec2_ami" {
+    most_recent = true
+    owners = ["amazon"]
+
+filter {
+    name   = "name"
+    values = ["Amazon Linux 2 Kernel 5.10 AMI 2.0.20220426.0 x86_64 HVM gp2"]
+}
+}
+variable "ec2_name" {
+
+default = "terraform1_instance"
+}
+
+
+    output "ec2_ip" {
+    value = aws_instance.app_server
+}
+resource "aws_instance" "web" {
+  
+
+  provisioner "local-exec" {
+    command = "echo The server's IP address is ${self.public.id}"
+  }
+
+  provisioner "file" {
+  source      = "conf/myapp.conf"
+  destination = "/etc/myapp.conf"
+
+  connection {
+    type     = "ssh"
+    user     = "root"
+    password = "${var.root_password}"
+    host     = "${var.host}"
+  }
+}
+    
+ }
